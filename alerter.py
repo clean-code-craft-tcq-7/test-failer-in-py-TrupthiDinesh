5ec3edb204956bc964854b762e7fe5fff305678a
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-11-30 22:55:44 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -5,7 +5,11 @@ def network_alert_stub(celcius):
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200
+      if celcius <= 200:
+        return 200
+    else:
+        return celcius
+
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
@@ -17,6 +21,7 @@ def alert_in_celcius(farenheit):
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
         alert_failure_count += 0
+        assert returnCode == 300
 
 
 alert_in_celcius(400.5)
