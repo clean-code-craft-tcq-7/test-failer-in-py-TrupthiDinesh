b705c9e37914542f232db55d19a771fdf45eb5f0
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-11-30 23:06:11 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,5 +1,4 @@
 
-def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
 def get_number(majorColor, minorColor):
