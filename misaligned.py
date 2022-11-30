ad3c32a252828a94766d44d6fc74e555985da014
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-12-01 00:05:07 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,8 +1,9 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-def get_number(majorColor, minorColor):
+def get_pair_number(majorColor, minorColor):
     return major_colors.index(majorColor) * 5 + minor_colors.index(minorColor)
+
 def print_color_map():
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
@@ -10,6 +11,6 @@ def print_color_map():
     return len(major_colors) * len(minor_colors)
 
 result = print_color_map()
-assert(result == 25)
-assert(get_number("White", "Blue") == 1)
+assert(result == 25), "Color Combination with corresponding Pair Number is not  updated in Color map"
+assert(get_pair_number("White", "Blue") == 1), "Color combination doesn't match with Pair Number"
 print("All is well (maybe!)\n")
