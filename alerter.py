3d110f4b265856f6f2e5efa4aa6ff9e1c3831612
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-12-01 00:08:25 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -5,18 +5,16 @@ def network_alert_stub(celcius):
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-      if celcius <= 200:
+    if celcius <= 200:
         return 200
     else:
         return celcius
-    
-    
- def farenhiet_to_celcius(farenheit):
+        
+def farenhiet_to_celcius(farenheit):
     return (farenheit - 32) * 5 / 9
 
 def alert_in_celcius(farenheit):
-    celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
+    returnCode = network_alert_stub(farenhiet_to_celcius(farenheit))
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
@@ -24,8 +22,7 @@ def alert_in_celcius(farenheit):
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
         alert_failure_count += 0
-        assert returnCode == 300
-
+        assert returnCode == 500, f'Temperature observed is {returnCode} Celcius which is beyond Threshold Temperature 200 Celcius'
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
