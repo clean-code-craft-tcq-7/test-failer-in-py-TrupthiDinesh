f4829d90985728bffee893caab21171de72565fb
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-12-07 22:05:49 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -12,5 +12,4 @@ def print_color_map():
 
 result = print_color_map()
 assert(result == 25), "Color Combination with corresponding Pair Number is not  updated in Color map"
-assert(get_pair_number("White", "Blue") == 1), "Color combination doesn't match with Pair Number"
 print("All is well (maybe!)\n")
